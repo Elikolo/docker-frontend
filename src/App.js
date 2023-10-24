7b79a6e48d34349d6d808a672c103e6695360a79
Aimé SAWADOGO <aime@guesttoguest.com>
2023-10-24 16:09:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          je teste un petit changement
         </p>
         <a
           className="App-link"
